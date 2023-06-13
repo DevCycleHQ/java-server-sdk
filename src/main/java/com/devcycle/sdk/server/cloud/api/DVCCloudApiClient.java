cd08533daf0275d06854f6e916226ea2b75fe6a4
chris-hoefgen <chris.hoefgen@taplytics.com>
2023-06-13 08:27:45 -0700

    trying out an example with certs as part of the okbuilder

--- src/main/java/com/devcycle/sdk/server/cloud/api/DVCCloudApiClient.java
+++ src/main/java/com/devcycle/sdk/server/cloud/api/DVCCloudApiClient.java
@@ -2,7 +2,9 @@ package com.devcycle.sdk.server.cloud.api;
 
 import com.devcycle.sdk.server.cloud.model.DVCCloudOptions;
 import com.devcycle.sdk.server.common.api.IDVCApi;
+import com.devcycle.sdk.server.common.exception.DVCException;
 import com.devcycle.sdk.server.common.interceptor.AuthorizationHeaderInterceptor;
+import com.devcycle.sdk.server.common.model.HttpResponseCode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
@@ -10,6 +12,11 @@ import okhttp3.*;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+import java.security.*;
 import java.util.Objects;
 
 public final class DVCCloudApiClient {
@@ -42,6 +49,22 @@ public final class DVCCloudApiClient {
   public DVCCloudApiClient(String apiKey, DVCCloudOptions options) {
     this(options);
     okBuilder.addInterceptor(new AuthorizationHeaderInterceptor(apiKey));
+
+    KeyStore keyStore = null;//options.getCustomeCerts(); //todo Load from options
+    String keyStorePass = "keystore_pass"; //todo Load from options
+    SSLContext sslContext = null;
+    try {
+      // example code pulled from: https://stackoverflow.com/questions/23103174/does-okhttp-support-accepting-self-signed-ssl-certs
+      sslContext = SSLContext.getInstance("SSL");
+      TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+      trustManagerFactory.init(keyStore);
+      KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+      keyManagerFactory.init(keyStore, keyStorePass.toCharArray());
+      sslContext.init(keyManagerFactory.getKeyManagers(),trustManagerFactory.getTrustManagers(), new SecureRandom());
+      okBuilder.sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustManagerFactory.getTrustManagers()[0]);
+    } catch (GeneralSecurityException e) {
+      throw new RuntimeException("Error while configuring SSL Certificates", e);
+    }
   }
 
   public IDVCApi initialize() {
