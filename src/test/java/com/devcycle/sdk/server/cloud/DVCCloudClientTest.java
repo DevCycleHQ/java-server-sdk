55141e8031bd025305221e2290462536e4613038
Chris Hoefgen <58341173+chris-hoefgen@users.noreply.github.com>
2023-06-16 11:25:47 -0700

    bumping version numbers (#79)

--- src/test/java/com/devcycle/sdk/server/cloud/DVCCloudClientTest.java
+++ src/test/java/com/devcycle/sdk/server/cloud/DVCCloudClientTest.java
@@ -200,6 +200,6 @@ public class DVCCloudClientTest {
         Assert.assertEquals("Java", user.getPlatform());
         Assert.assertEquals(PlatformData.SdkTypeEnum.SERVER, user.getSdkType());
         Assert.assertNotNull(user.getPlatformVersion());
-        Assert.assertEquals("1.5.2", user.getSdkVersion());
+        Assert.assertEquals("1.6.0", user.getSdkVersion());
     }
 }
