3933be82ad5f6e583d6f87a222f76c35a8e5605f
chris-hoefgen <chris.hoefgen@taplytics.com>
2023-06-16 10:44:31 -0700

    bumping version numbers

--- src/main/java/com/devcycle/sdk/server/common/model/PlatformData.java
+++ src/main/java/com/devcycle/sdk/server/common/model/PlatformData.java
@@ -46,7 +46,7 @@ public class PlatformData {
 
     @Schema(description = "DevCycle SDK Version")
     @Builder.Default
-    private String sdkVersion = "1.5.2";
+    private String sdkVersion = "1.6.0";
 
     @Schema(description = "Hostname where the SDK is running")
     private String hostname;
