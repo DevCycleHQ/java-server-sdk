cb69122fbd410359e04c95dedbf3313dac22cff4
DevCycle Automation <github-tracker-bot@taplytics.com>
2023-08-10 20:59:27 +0000

    Release 2.0.0

--- src/main/java/com/devcycle/sdk/server/common/model/PlatformData.java
+++ src/main/java/com/devcycle/sdk/server/common/model/PlatformData.java
@@ -44,7 +44,7 @@ public class PlatformData {
 
     @Schema(description = "DevCycle SDK Version")
     @Builder.Default
-    private String sdkVersion = "1.8.3";
+    private String sdkVersion = "2.0.0";
 
     @Schema(description = "Hostname where the SDK is running")
     private String hostname;
