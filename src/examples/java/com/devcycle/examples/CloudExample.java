6e5e888b190cbd0d400de46beaa3366c917dd14b
Rob ODwyer <126593209+rob-odwyer@users.noreply.github.com>
2023-08-16 08:08:33 -0700

    ci: Add simplified examples and a workflow to run them on PRs (#95)
    * add simplified examples and a workflow to run them on PRs
    
    * fix gradle build
    
    * update to new DevCycle prefix
    
    * update variable names to remove dvc

---
+++ src/examples/java/com/devcycle/examples/CloudExample.java
@@ -0,0 +1,49 @@
+package com.devcycle.examples;
+
+import com.devcycle.sdk.server.cloud.api.DevCycleCloudClient;
+import com.devcycle.sdk.server.cloud.model.DevCycleCloudOptions;
+import com.devcycle.sdk.server.common.exception.DevCycleException;
+import com.devcycle.sdk.server.common.model.DevCycleUser;
+
+public class CloudExample {
+    public static String VARIABLE_KEY = "test-boolean-variable";
+
+    public static void main(String[] args) throws InterruptedException {
+        String server_sdk_key = System.getenv("DEVCYCLE_SERVER_SDK_KEY");
+        if (server_sdk_key == null) {
+            System.err.println("Please set the DEVCYCLE_SERVER_SDK_KEY environment variable");
+            System.exit(1);
+        }
+
+        // Create user object
+        DevCycleUser user = DevCycleUser.builder()
+                .userId("SOME_USER_ID")
+                .build();
+
+        // The default value can be of type string, boolean, number, or JSON
+        Boolean defaultValue = false;
+
+        DevCycleCloudOptions options = DevCycleCloudOptions.builder().build();
+
+        // Initialize DevCycle Client
+        DevCycleCloudClient client = new DevCycleCloudClient(server_sdk_key, options);
+
+        // Fetch variable values using the identifier key, with a default value and user
+        // object
+        // The default value can be of type string, boolean, number, or JSON
+        Boolean variableValue = false;
+        try {
+            variableValue = client.variableValue(user, VARIABLE_KEY, defaultValue);
+        } catch(DevCycleException e) {
+            System.err.println("Error fetching variable value: " + e.getMessage());
+            System.exit(1);
+        }
+
+        // Use variable value
+        if (variableValue) {
+            System.err.println("feature is enabled");
+        } else {
+            System.err.println("feature is NOT enabled");
+        }
+    }
+}
---
+++ src/examples/java/com/devcycle/examples/LocalExample.java
@@ -0,0 +1,50 @@
+package com.devcycle.examples;
+
+import com.devcycle.sdk.server.local.api.DevCycleLocalClient;
+import com.devcycle.sdk.server.local.model.DevCycleLocalOptions;
+import com.devcycle.sdk.server.common.model.DevCycleUser;
+
+public class LocalExample {
+    public static String VARIABLE_KEY = "test-boolean-variable";
+
+    public static void main(String[] args) throws InterruptedException {
+        String server_sdk_key = System.getenv("DEVCYCLE_SERVER_SDK_KEY");
+        if (server_sdk_key == null) {
+            System.err.println("Please set the DEVCYCLE_SERVER_SDK_KEY environment variable");
+            System.exit(1);
+        }
+
+        // Create user object
+        DevCycleUser user = DevCycleUser.builder()
+                .userId("SOME_USER_ID")
+                .build();
+
+        // The default value can be of type string, boolean, number, or JSON
+        Boolean defaultValue = false;
+
+        DevCycleLocalOptions options = DevCycleLocalOptions.builder().configPollingIntervalMs(60000)
+                .disableAutomaticEventLogging(false).disableCustomEventLogging(false).build();
+
+        // Initialize DevCycle Client
+        DevCycleLocalClient client = new DevCycleLocalClient(server_sdk_key, options);
+
+        for (int i = 0; i < 10; i++) {
+            if(client.isInitialized()) {
+                break;
+            }
+            Thread.sleep(500);
+        }
+
+        // Fetch variable values using the identifier key, with a default value and user
+        // object
+        // The default value can be of type string, boolean, number, or JSON
+        Boolean variableValue = client.variableValue(user, VARIABLE_KEY, defaultValue);
+
+        // Use variable value
+        if (variableValue) {
+            System.err.println("feature is enabled");
+        } else {
+            System.err.println("feature is NOT enabled");
+        }
+    }
+}
