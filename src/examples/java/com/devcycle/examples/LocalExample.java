c3c6a810025712593987ff582f26bfd250250d7f
Parth Suthar <parth.suthar@taplytics.com>
2025-07-23 16:29:21 -0400

    revert local example testing file

--- src/examples/java/com/devcycle/examples/LocalExample.java
+++ src/examples/java/com/devcycle/examples/LocalExample.java
@@ -1,18 +1,13 @@
 package com.devcycle.examples;
 
-import java.util.Optional;
-
 import com.devcycle.sdk.server.common.logging.SimpleDevCycleLogger;
 import com.devcycle.sdk.server.common.model.DevCycleEvent;
 import com.devcycle.sdk.server.common.model.DevCycleUser;
-import com.devcycle.sdk.server.common.model.EvalHook;
-import com.devcycle.sdk.server.common.model.HookContext;
-import com.devcycle.sdk.server.common.model.Variable;
 import com.devcycle.sdk.server.local.api.DevCycleLocalClient;
 import com.devcycle.sdk.server.local.model.DevCycleLocalOptions;
 
 public class LocalExample {
-    public static String VARIABLE_KEY = "example-text";
+    public static String VARIABLE_KEY = "test-boolean-variable";
 
     public static void main(String[] args) throws InterruptedException {
         String server_sdk_key = System.getenv("DEVCYCLE_SERVER_SDK_KEY");
@@ -23,11 +18,11 @@ public class LocalExample {
 
         // Create user object
         DevCycleUser user = DevCycleUser.builder()
-                .userId("j_test")
+                .userId("SOME_USER_ID")
                 .build();
 
         // The default value can be of type string, boolean, number, or JSON
-        String defaultValue = "false";
+        Boolean defaultValue = false;
 
         DevCycleLocalOptions options = DevCycleLocalOptions.builder()
                 .customLogger(new SimpleDevCycleLogger(SimpleDevCycleLogger.Level.DEBUG))
@@ -35,31 +30,6 @@ public class LocalExample {
 
         // Initialize DevCycle Client
         DevCycleLocalClient client = new DevCycleLocalClient(server_sdk_key, options);
-
-        client.addHook(new EvalHook<String>() {
-            @Override
-            public Optional<HookContext<String>> before(HookContext<String> ctx) {
-                System.out.println("before");
-                System.out.println(ctx.getMetadata().getProject().getKey());
-                System.out.println(ctx.getMetadata().getEnvironment().getKey());
-                return Optional.of(ctx);
-            }
-
-            @Override
-            public void after(HookContext<String> ctx, Variable<String> variable) {
-                System.out.println("after");
-                System.out.println(variable.getValue());
-                System.out.println(ctx.getMetadata().getProject().getKey());
-                System.out.println(ctx.getMetadata().getEnvironment().getKey());
-            }
-
-            @Override
-            public void onFinally(HookContext<String> ctx, Optional<Variable<String>> variable) {
-                System.out.println("finally");
-                System.out.println(ctx.getMetadata().getProject().getKey());
-                System.out.println(ctx.getMetadata().getEnvironment().getKey());
-            }
-        });
 
         for (int i = 0; i < 10; i++) {
             if (client.isInitialized()) {
@@ -71,13 +41,13 @@ public class LocalExample {
         // Fetch variable values using the identifier key, with a default value and user
         // object
         // The default value can be of type string, boolean, number, or JSON
-        String variableValue = client.variableValue(user, VARIABLE_KEY, defaultValue);
+        Boolean variableValue = client.variableValue(user, VARIABLE_KEY, defaultValue);
 
         // Use variable value
-        if (variableValue.equals("true")) {
+        if (variableValue) {
             System.out.println("feature is enabled");
         } else {
-            System.out.println("feature is NOT enabled: " + variableValue);
+            System.out.println("feature is NOT enabled");
         }
 
         DevCycleEvent event = DevCycleEvent.builder().type("local-test").build();
