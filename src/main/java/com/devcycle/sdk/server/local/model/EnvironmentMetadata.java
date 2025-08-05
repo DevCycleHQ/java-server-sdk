2cbdef54272263df65eb5d5ea84d9db5d9e53757
Parth Suthar <parth.suthar@taplytics.com>
2025-08-05 10:39:52 -0400

    remove no args decorator

--- src/main/java/com/devcycle/sdk/server/local/model/EnvironmentMetadata.java
+++ src/main/java/com/devcycle/sdk/server/local/model/EnvironmentMetadata.java
@@ -2,15 +2,13 @@ package com.devcycle.sdk.server.local.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EnvironmentMetadata {
     @JsonProperty("id")
-    public String id;
+    public final String id;
     @JsonProperty("key")
-    public String key;
+    public final String key;
 }
--- src/main/java/com/devcycle/sdk/server/local/model/ProjectMetadata.java
+++ src/main/java/com/devcycle/sdk/server/local/model/ProjectMetadata.java
@@ -1,16 +1,14 @@
 package com.devcycle.sdk.server.local.model;
 
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ProjectMetadata {
     @JsonProperty("id")
-    public String id;
+    public final String id;
     @JsonProperty("key")
-    public String key;
+    public final String key;
 }
